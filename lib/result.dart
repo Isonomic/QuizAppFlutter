82cd8db37fff89612095887fa2e6bb6f437d3f47
Ryan <designteam1234@gmail.com>
2019-08-16 11:45:28 -0600

    initial commit

---
+++ lib/result.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetHandler;
+
+  Result(this.resultScore, this.resetHandler);
+
+  String get resultPhrase {
+    String resultText;
+    if (resultScore <= 8) {
+      resultText = 'You are awesome and innocent!';
+    } else if (resultScore <= 12) {
+      resultText = 'Pretty likeable!';
+    } else if (resultScore <= 16) {
+      resultText = 'You are ... strange?!';
+    } else {
+      resultText = 'You are so bad!';
+    }
+    return resultText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          children: <Widget>[
+            Text(
+              resultPhrase,
+              style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 78,
+            ),
+            FlatButton(
+              child: Text(
+                'Restart Quiz!',
+                style: TextStyle(fontSize: 40),
+              ),
+              textColor: Colors.lightBlue[900],
+              onPressed: resetHandler,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
